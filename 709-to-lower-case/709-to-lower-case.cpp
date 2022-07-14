fdef8452ccec147fa2fddfb71c99b6a9271da14b
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-07-14 11:53:20 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (7.85%) - LeetHub

---
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string toLowerCase(string s) {
+        string ans = "";
+        
+        for(int  i  =0 ;i<s.size();i++)
+        {
+            ans  += tolower(s[i]);
+        }
+        return ans;
+    }
+};
