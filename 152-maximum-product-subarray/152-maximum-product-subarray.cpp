db30912970801c315983059154497d62361898c1
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-08-02 23:53:52 +0530

    Time: 9 ms (70.51%), Space: 13.8 MB (26.08%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-       int mx = 1;                           // to store continuous curr max multiplication
-    int mn = 1;                           // to store continuous curr min multiplication
-    int ans = INT_MIN;                    // to store ans
+       int mx = 1;                          
+    int mn = 1;                       
+    int ans = INT_MIN;                   
     
-    // we check every element is it's negative then swap mx and mn
+    
     for(int i=0;i<nums.size();i++){
         if(nums[i]<0){
             swap(mx,mn);
