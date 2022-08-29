0a3443999c2dd1ce4b67c44c7ad7ea9ed30c61ae
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-08-29 20:11:07 +0530

    Time: 25 ms (5.23%), Space: 14.6 MB (97.87%) - LeetHub

---
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -0,0 +1,79 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+          if(list1 == NULL)
+          {
+              return list2;
+          }
+        if(list2 == NULL)
+        {
+            return  list1;
+        }
+    
+      if(list1->val <list2->val)
+      {
+          list1->next  =  mergeTwoLists(list1->next ,list2);
+          return list1;
+      }
+        else{
+            list2->next  =   mergeTwoLists( list1 , list2->next);
+            return list2;
+        }
+        
+        }
+};
+        
+        
+//         ListNode* ft = nullptr;
+//         ListNode* fh = NULL;
+        
+//         while(list1 !=NULL && list2 !=NULL)
+//         {
+//             if(ft== NULL && fh==NULL)
+//             {
+//             if(list1->val > list2->val)
+//             {
+//                ft = list2;
+//                fh = list2;
+//                list2  = list2->next;
+//             }
+//           else
+//           {
+//                ft  =  list1;
+//                 fh =  list1;
+//               list1  = list1->next;
+//           }
+            
+//             }
+//         if(list1->val > list2->val)
+            
+//         {
+//             ft->next  =  list2;
+//             ft  =  ft->next;
+//             list2 =  list2->next;
+//         }
+//        else{
+//            ft->next = list1;
+//            ft =  ft->next;
+//            list1 = list1->next;
+//           }
+//             }
+//         if(list1 != NULL){
+//             ft->next  =  list1;
+//         }
+//         if(list2 != NULL){
+//             ft->next = list2;
+//         }
+              
+//         return fh;
+    
