5c4e250ae9ff9f033dc286908843641bf70613f3
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-09-15 01:09:31 +0530

    Time: 3 ms, Memory: 7.1 MB - LeetHub

--- rotate-image/rotate-image.cpp
+++ rotate-image/rotate-image.cpp
@@ -4,7 +4,7 @@ public:
        int  n = matrix.size();
     for(int  i =0;i<n;i++)
 
-        for(int j=0;j<i;j++)
+        for(int j=0;j<=i;j++)
         
             
             swap(matrix[i][j],matrix[j][i]);
