23c53f39f040e27b9157403788f8f68b196955f9
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-09-16 21:05:58 +0530

    Time: 0 ms, Memory: 6.3 MB - LeetHub

--- implement-strstr/implement-strstr.cpp
+++ implement-strstr/implement-strstr.cpp
@@ -1,7 +1,12 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int pos  =  haystack.find(needle);
-        return pos;
+       int  n = haystack.length();
+       int m=   needle.length();
+       for(int i=0;i<=n-m;i++){
+        string temp = haystack.substr(i,m);
+        if(temp == needle) return i;
+    }
+    return -1;
     }
 };
