5c761f54ead62f7236c18421759932b5f1717bc2
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-09-19 18:38:57 +0530

    Time: 4 ms, Memory: 6.4 MB - LeetHub

---
+++ valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        
+        for(int i =0;i<s.size();i++)
+        {
+            if(s[i] == '('  || s[i] == '['  || s[i] == '{')
+            {
+                st.push(s[i]);
+                
+            }
+        else
+        {
+            if(st.empty() || (st.top() == '(' && s[i] != ')')  || (st.top() == '{' && s[i] != '}') || (st.top() == '[' && s[i] != ']') ) return false;
+            
+            else 
+                st.pop();
+            
+        }
+            
+    }
+        return  st.empty();
+    }
+};
