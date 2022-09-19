b26cb96b56d860b5fe75e4e1d2b8fa17607152a0
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2022-09-19 17:01:05 +0530

    Time: 0 ms, Memory: 5.9 MB - LeetHub

--- number-of-1-bits/number-of-1-bits.cpp
+++ number-of-1-bits/number-of-1-bits.cpp
@@ -5,8 +5,8 @@ public:
         
         while(n!=0)
               {
-                  n= n&(n-1);
-                  cnt++;
+                 if(n&1) cnt++;
+                  n>>=1;
         }
         return cnt;
     }
