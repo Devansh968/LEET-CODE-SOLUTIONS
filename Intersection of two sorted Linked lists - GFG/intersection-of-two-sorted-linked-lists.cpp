9f1400ce6038cf2e6854411d7564cb1ec4057869
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2023-10-08 15:44:52 +0530

    Added solution - LeetHub

---
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
@@ -0,0 +1,123 @@
+//{ Driver Code Starts
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+Node* inputList(int size)
+{
+    Node *head, *tail;
+    int val;
+    
+    cin>>val;
+    head = tail = new Node(val);
+    
+    while(--size)
+    {
+        cin>>val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+}
+
+Node* findIntersection(Node* head1, Node* head2);
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>> n >> m;
+	    
+	    Node* head1 = inputList(n);
+	    Node* head2 = inputList(m);
+	    
+	    Node* result = findIntersection(head1, head2);
+	    
+	    printList(result);
+	    cout<< endl;
+	}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/* The structure of the Linked list Node is as follows:
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node(int val)
+    {
+        data=val;
+        next=NULL;
+    }
+};
+
+*/
+
+Node* findIntersection(Node* head1, Node* head2)
+{
+    // Your Code Here
+    
+    Node*  ptr1  =  head1;
+    Node*  ptr2  = head2;
+    Node*  head = NULL;
+    Node* curr  = NULL;
+     
+    while(ptr1 && ptr2){
+        if(ptr1->data == ptr2->data)
+{        
+        if(head== NULL){
+            
+            Node* t  = new Node(ptr1->data);
+            head =t;
+            curr = t;
+            
+        }
+        else{
+            Node* t  =  new Node(ptr1->data);
+            curr->next  =t;
+            curr = curr->next;
+        }
+        ptr1 = ptr1->next;
+        ptr2 =  ptr2->next;
+}
+     else{
+         if(ptr1->data < ptr2->data){
+             ptr1 = ptr1->next;
+         }
+         else{
+             ptr2 = ptr2->next;
+         }
+     }
+}
+return head;
+        
+    }
