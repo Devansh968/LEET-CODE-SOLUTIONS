087d978db274b445c18ba9a9d1e4829f18f53454
Devanshu Pathak <90167731+Devansh968@users.noreply.github.com>
2023-10-12 14:12:40 +0530

    Added solution - LeetHub

---
+++ Reverse Coding - GFG/reverse-coding.cpp
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int sumOfNaturals(int n) {
+        // code here
+        int temp  = 1e9+7;
+          long ans  = long(n)*long(n+1);
+         ans  = ans/2;
+         return ans%temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        
+        cin>>n;
+
+        Solution ob;
+        cout << ob.sumOfNaturals(n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
